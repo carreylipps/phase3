8a311373ee522c9c8405b533055cb4c6de64c5a8
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-04-30 21:57:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var fs = require("fs");
 const  xml2js  = require('xml2js');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const { parseStringPromise } = require('xml2js');
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
